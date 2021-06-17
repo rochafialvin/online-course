3c444ea269d31d9a00b4425a8959b61ebef5f4f3
rochafialvin <rochafi.dev@gmail.com>
2021-06-17 21:29:00 +0700

    NEW Reverse Word

--- Session 6 - function/Exercise/Solutions/6. Reverse Word/index.js
+++ Session 6 - function/Exercise/Solutions/6. Reverse Word/index.js
@@ -14,7 +14,7 @@
   ["Hai", "aku", "Joan", "Mir"]
 
   // map
-  "iaH", "uka", "naoJ", "riM"][
+  ["iaH", "uka", "naoJ", "riM"]
 
       "Hai"
 
@@ -31,6 +31,48 @@
   iaH uka naoJ riM
 
 */
+
+
+const reverseWords = (word) => {
+  // word : "Hai aku Joan Mir"
+  const words = word.split(" ");
+
+  // words : ["Hai", "aku", "Joan", "Mir"]
+  // wordFinal : [ "iaH", "uka", "naoJ", "riM" ]
+  let wordsFinal = words.map((word) => {
+    // word : "Hai"
+
+    // word : ["H", "a", "i"]
+    word = word.split("");
+
+    // word : ["i", "a", "H"]
+    word.reverse();
+
+    // word : "iaH"
+    word = word.join("");
+
+    return word;
+  });
+
+  wordsFinal = wordsFinal.join(" ");
+
+  return wordsFinal;
+};
+
+const reverseWords = (word) => {
+  // ["Hai", "aku", "Joan", "Mir"]
+  let words = word.split(" ");
+
+  // wordFinal = [ "iaH", "uka", "naoJ", "riM" ]
+  let wordsFinal = words.map((val) => {
+    val = val.split("").reverse().join("");
+
+    return val;
+  });
+
+  // wordsFinal = iaH uka naoJ riM
+  return wordsFinal.join(" ");
+};
 
 const reverseWords = (word) => {
   let words = word.split(" ");
